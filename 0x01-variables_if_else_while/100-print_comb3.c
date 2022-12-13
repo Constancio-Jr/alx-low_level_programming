4ffa1cd8ed5b5842606fd25578c53231ba3d7167
Constancio Junior <constancioalbino@gmail.com>
2022-12-12 16:09:25 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers betewwn 0 to 89
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, e;
+
+	i = 48;
+	e = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (w != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 58 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
