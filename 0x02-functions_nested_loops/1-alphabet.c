2fa04710f6cbd2f733e2a118cc92ee5aee7e4d46
Constancio Junior <constancioalbino@gmail.com>
2022-12-14 09:57:15 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - prints the alphabet in lowercase
  *
- * Return: Always 0
  */
-voif print_alphabet(void)
+void print_alphabet(void)
 {
 	char la;
 
