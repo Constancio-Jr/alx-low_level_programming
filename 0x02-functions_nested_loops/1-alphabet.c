0be20d5eea877da2ec5ed1ee624064a9722c2372
Constancio Junior <constancioalbino@gmail.com>
2022-12-14 09:48:19 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+voif print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	putchar('\n');
+}
